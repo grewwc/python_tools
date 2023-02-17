ef717c3885b29ac5708b6a4bfb69bb9dfe3fd8c7
grewwc <wangwenchao.wwc@alibaba-inc.com>
2023-02-17 20:43:42 +0800

    ratelimiter

---
+++ utils/rate_limiter.py
@@ -0,0 +1,33 @@
+import time
+import threading
+
+
+class SimpleRateLimiter:
+    def __init__(self, rate, cap=None, init_token=None):
+        self._rate = rate
+        self._prev_time = time.perf_counter()
+        self._cap = cap if cap is not None else self._rate*2
+        self._l = threading.Lock()
+        self.token = init_token if min(init_token, self._cap) is not None else 0
+
+    def ok(self, n=1):
+        with self._l:
+            cur = time.perf_counter()
+            acc = (cur-self._prev_time)*self._rate
+            self.token += acc
+            self.token = min(self.token, self._cap)
+            self._prev_time = cur
+            if self.token >= n:
+                self.token -= n
+                return True
+            return False
+
+    def aquire(self, n=1, timeout=None):
+        start = time.perf_counter()
+
+        while True:
+            if self.ok(n):
+                return True
+            if timeout is not None and time.perf_counter()-start >= timeout:
+                return False
+            time.sleep(0.01)
